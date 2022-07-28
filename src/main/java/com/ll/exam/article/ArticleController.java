fecf52d88831a38b89eee25e9e2d4bb1e91d36f8
jootang2 <joohwan.song15b@gmail.com>
2022-07-29 01:00:41 +0900

    게시물 수정, 삭제, 등록 이후 replace로 이동

--- src/main/java/com/ll/exam/article/ArticleController.java
+++ src/main/java/com/ll/exam/article/ArticleController.java
@@ -29,7 +29,7 @@ public class ArticleController {
 
         long id = articleService.write(title, body);
 
-        rq.println("%d번 게시물이 생성 되었습니다.".formatted(id));
+        rq.replace("/usr/article/detail/free/%d".formatted(id),"%d번 게시물이 생성 되었습니다.".formatted(id));
     }
 
     public void showDetail(Rq rq) {
@@ -68,8 +68,7 @@ public class ArticleController {
 
         articleService.delete(id);
 
-        rq.println("%d번 게시물이 삭제되었습니다".formatted(id));
-        rq.println("<div><a href=\"/usr/article/list/free\">리스트로 이동</a></div>".formatted(id));
+        rq.replace("/usr/article/list/free" , "%d번 게시물이 삭제되었습니다".formatted(id));
     }
 
     public void showModify(Rq rq) {
@@ -98,8 +97,8 @@ public class ArticleController {
         String body = rq.getParam("body","");
 
         articleService.modify(id, title, body);
-        rq.println("%d번 게시물이 수정되었습니다".formatted(id));
-        rq.println("<div><a href=\"/usr/article/detail/free/%d\">수정된 글로 이동</a></div>".formatted(id));
+
+        rq.replace("/usr/article/detail/free/%d".formatted(id),"%d번 게시물이 수정되었습니다".formatted(id));
 
     }
 }
