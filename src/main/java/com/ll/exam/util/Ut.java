5bc513bb1f3b698a43bda0cbcf8df074a5e600f4
jootang2 <joohwan.song15b@gmail.com>
2022-07-29 14:38:37 +0900

    Ut.json.toJsonStr -> ut.json.toStr 로 메소드 명 변겅

--- src/main/java/com/ll/exam/util/Ut.java
+++ src/main/java/com/ll/exam/util/Ut.java
@@ -2,10 +2,11 @@ package com.ll.exam.util;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ll.exam.article.dto.ArticleDto;
 
 public class Ut {
     public static class json {
-        public static String toJsonStr(Object obj, String defaultValue){
+        public static String toStr(Object obj, String defaultValue){
 
             ObjectMapper om =new ObjectMapper();
 
@@ -16,5 +17,6 @@ public class Ut {
             }
 
         }
+
     }
 }
